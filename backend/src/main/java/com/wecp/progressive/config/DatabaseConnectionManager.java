cd1300bee08ba796630727933ceb828745161cdd
username <wecp-dummy@gmail.com>
2025-09-20 11:42:08 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/config/DatabaseConnectionManager.java
+++ backend/src/main/java/com/wecp/progressive/config/DatabaseConnectionManager.java
@@ -1,12 +1,12 @@
 package com.wecp.progressive.config;
-
+import java.io.IOException;;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import io.jsonwebtoken.io.IOException;
+
 
 public class DatabaseConnectionManager {
     private static final Properties properties = new Properties();
