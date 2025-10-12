3b370af12255e450d6683efc5eb718950cddae01
shankar56789 <148109563+shankar56789@users.noreply.github.com>
2025-10-12 10:57:05 +0530

    Delete backend directory

--- backend/src/main/java/com/wecp/progressive/exception/StudentAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.wecp.progressive.exception;
-
-public class StudentAlreadyExistsException extends RuntimeException{
-    public StudentAlreadyExistsException(String msg){
-        super(msg);
-    }
-}
