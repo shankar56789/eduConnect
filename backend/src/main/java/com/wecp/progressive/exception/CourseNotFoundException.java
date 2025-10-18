c7f2ea16d0fdb831aeaff01fad117074aaf926b8
shankar56789 <148109563+shankar56789@users.noreply.github.com>
2025-10-18 15:14:32 +0530

    Delete backend directory

--- backend/src/main/java/com/wecp/progressive/exception/CourseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.wecp.progressive.exception;
-
-public class CourseNotFoundException extends RuntimeException{
-    public CourseNotFoundException(String msg){
-        super(msg);
-    }
-}
--- backend/src/main/java/com/wecp/progressive/exception/TeacherAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.wecp.progressive.exception;
-
-public class TeacherAlreadyExistsException extends RuntimeException{
-    public TeacherAlreadyExistsException(String msg){
-        super(msg);
-    }
-}
